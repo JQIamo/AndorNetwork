47df734d085a3e847c49e65b8917c03b0b5bd882
Yanda Geng <gengyd@umd.edu>
2022-12-07 20:52:12 -0500

    Fixed C++11 compilation issues

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,11 @@ int main(int argc, char* argv[]) {
         Ice::CommunicatorHolder ich;
         auto adapter = ich->createObjectAdapterWithEndpoints("AndorNetworkAdapter", "default -p 10000");
         auto object = std::make_shared<AndorI>();
-        adapter->add(object, ich->stringToIdentity("AndorNetwork"));
+        adapter->add(object, Ice::stringToIdentity("AndorNetwork"));
         adapter->activate();
         ich->waitForShutdown();
     } catch (const std::exception& e) {
-        cerr << e.what() << endl;
+        std::cerr << e.what() << std::endl;
         return 1;
     }
     return 0;
